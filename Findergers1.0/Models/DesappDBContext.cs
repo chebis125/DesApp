4e889559d7d7bc2f0f38ffc3f3536484f7c17957
Sebastian Martinez <martinezpulidojohansebastian@gmail.com>
2022-11-11 19:26:09 -0500

    Claud

--- Findergers1.0/Models/DesappDBContext.cs
+++ Findergers1.0/Models/DesappDBContext.cs
@@ -24,7 +24,7 @@ namespace Findergers1._0.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DesappDB.mssql.somee.com;Database=DesappDB;user id=MindafyChat_SQLLogin_1;password=kz7exwvjsb;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer("Server=35.237.0.152;Database=DesappDB;user=sqlserver;password=desapp;MultipleActiveResultSets=true");
             }
         }
 
--- Findergers1.0/Models/Missing.cs
+++ Findergers1.0/Models/Missing.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
 
 namespace Findergers1._0.Models
 {
     public partial class Missing
     {
         public int IdMissing { get; set; }
-        [Display(Name = "Name")]
         public string? NameMissing { get; set; }
-        [Display(Name = "Age")]
         public int? AgeMissing { get; set; }
-        [Display(Name = "Description")]
         public string? DescriptionMissing { get; set; }
-        [Display(Name = "Date of disappearance")]
         public DateTime? DateMissing { get; set; }
         public string? ImageMissing { get; set; }
-        [Display(Name = "Last Location")]
         public string? LastlocationMissing { get; set; }
     }
 }
